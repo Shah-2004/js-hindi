2ac819826e269c7feed3c043fe2a187bcb09f91e
Shahrukh Khan <mr.shahrukh129@gmail.com>
2025-05-11 12:36:46 +0000

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144553
+let accountEmail = "srk@gmail.com"
+var accountPasssword = "12345"
+accountCity = "jaipur"   // ek variable is tarah se bhi decraile ho sakta h
+let accountState;    // agr m isme value put ni krta hu to js ise undefined manta h 
+
+// accountId = 2    // not allowed   hm constaint ko change ni kr skte
+
+// {} curli brases ko scope bhi kha jata h
+
+//  use var ki jagah let
+
+accountEmail = "hc@hc.com"
+accountPasssword = "5343"
+accountCity = "Ajmer"
+
+console.log(accountId);
+
+/* 
+
+*/
+ 
+console.table([accountId, accountEmail, accountPasssword, accountCity, accountState])
+
