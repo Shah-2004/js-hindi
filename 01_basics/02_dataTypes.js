c15e60b79912c275eee60ad4f1b46f09e69224cd
Shahrukh Khan <mr.shahrukh129@gmail.com>
2025-05-12 01:50:53 +0000

    data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not broweser
+
+console.log(3
+    + 3)  // code readability is imp its not good way to write code
+
+
+console.log("Shahrukh")
+
+
+let name = "SRK"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true / false  (ha / na)
+// null (its a standalone value h or null ek object h)  => standalone value (null mtlb khali/ empty h)
+// undefined => abhi value assign ni huwi h
+// symbol => use for uniqueness 
+
+
+// object 
+
+console.log(typeof "Shah");  // string
+console.log(typeof age);    // number
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
+
+
+
+
+
