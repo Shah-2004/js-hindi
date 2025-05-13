84e85d3350d780c2ae57786668c74ef41c817a08
Shahrukh Khan <mr.shahrukh129@gmail.com>
2025-05-13 01:52:58 +0000

    Operations

--- 01_basics/03_conversionOpretion.js
+++ 01_basics/03_conversionOpretion.js
@@ -1,3 +1,6 @@
+//  node 01_basics/03_conversionOpretion.js//  for run code
+
+
 //let score = 33
 // let score = "33abdsf"
 //let score = null
@@ -30,12 +33,44 @@
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
- 
+ /**************** Operations ***************/ 
 
+// let value = 3
+// let negValue = -value  
+// console.log(negValue);  // its print negative value
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  // 2 res di power 2
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);  // its print hello hitesh
+
+
+// console.log("1" + 2);  // its print 12  //string first me h sbhi str honge
+// console.log(1 + "2");  // its print 12
+// console.log("1" + 2 + 2);  // its print 122
+// console.log(1 + 2 + "2");  // its print 32 // 1+2 = 3 hoga phir 3+2 = 32 kyuki str last me h 
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);  // its print 101
 
 
 
