1ce726fd2073de7672b994cfeb598d5c7b7329b3
Shahrukh Khan <mr.shahrukh129@gmail.com>
2025-05-14 01:24:53 +0000

    dataType summery

---
+++ dataTypes.js
@@ -0,0 +1,69 @@
+// primitive
+//7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+
+
+// const id = Symbol('123')   // Symbol
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+
+//const BigNumber = 23443253634354675869n    // bigInt
+
+
+// Reference (Non Primitive)
+// Array, Objects, Functions   // dataTypes function hoga
+
+
+// const heros = ["shaktiman","naagraj" "doga"]
+
+let myObj = {
+    name: "shahrukh",        //object  //curly breses ke ander jo bhi values h vo obj h
+    age: 22,
+}
+
+
+const myFunction = function(){             // Function
+    console.log("Hello Shahrukh");
+}
+
+
+
+
+console.log(typeof myFunction);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
